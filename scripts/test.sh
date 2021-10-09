2adc03b60ef85981e3e8fa8b2877a837432d9f20
Rogelio Maldonado <rogermz.projects@gmail.com>
2021-10-09 11:27:04 -0500

    Agregando pruebas automatizadas

---
+++ scripts/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Mostrando GET /"
+curl http://localhost:3000/
+
+printf "\n\n"
+echo "Mostrando GET /saludo/<nombre>"
+curl http://localhost:3000/saludo/Roger
+
+printf "\n\n"
+echo "Fin de las pruebas... :)"
+
